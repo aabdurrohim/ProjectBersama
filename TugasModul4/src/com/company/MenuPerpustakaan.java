f1cdfdd08e01fd6f008d7242d0f2e95202c3ccc8
mbprayoga <102417378+mbprayoga@users.noreply.github.com>
2022-03-27 14:00:49 +0700

    class buat MenuPerpustakaan

--- TugasModul4/src/com/company/MenuPerpustakaan.java
+++ TugasModul4/src/com/company/MenuPerpustakaan.java
@@ -1,1 +1,12 @@
+package com.company;
 
+public class MenuPerpustakaan {
+    public void pinjamBuku(String pinjam, int counter){
+        if (counter == 0){
+            System.out.println("Anda akan meminjam buku "+pinjam);
+        }
+        else{
+            System.out.println("Anda tidak dapat meminjam lebih dari 1 buku sekaligus");
+        }
+    }
+}
